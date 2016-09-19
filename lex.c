5561ac1bb77e50fdb931ed6f6b3c56c7f4179b84
Jens Stimpfle <jfs@jstimpfle.de>
2016-09-19 14:05:11 +0200

    initial commit

---
+++ lex.c
@@ -0,0 +1,330 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lex.h"
+
+/* TODO: move away */
+#define WSL_UNREACHABLE() __builtin_unreachable()
+void *wsl_realloc(void *ptr, size_t nbytes)
+{
+        return realloc(ptr, nbytes);
+}
+
+enum wsl_lex_retcode
+wsl_lex_outbuf_store(
+        struct wsl_lex_outbuf *buffer,
+        unsigned char *begin,
+        unsigned char *end)
+{
+        unsigned char *dst;
+        size_t req;
+
+        if (buffer == NULL)
+                return WSL_LEX_OK;
+       
+        req = end - begin + 1;
+        if (WSL_LEX_OUTBUF_SIZE - buffer->size >= req) {
+                dst = &buffer->buf[buffer->size];
+                buffer->size += req;
+        } else {
+                void *ptr = wsl_realloc(buffer->bigbuf, req);
+                if (ptr == NULL)
+                        return WSL_LEX_OOM;
+                dst = ptr;
+                buffer->bigbuf = ptr;
+                buffer->bigsize = req;
+        }
+        memcpy(dst, begin, req-1);
+        dst[req-1] = 0;
+        return WSL_LEX_OK;
+}
+
+enum wsl_lex_retcode
+wsl_lex_token(
+        struct wsl_lex_cinfo *info,
+        struct wsl_lex_outbuf *buffer,
+        unsigned char *begin,
+        unsigned char **end_r)
+{
+        int r = WSL_LEX_OK;
+        unsigned char *c = begin;
+
+        switch (info->type) {
+        case WSL_LEX_STOP:
+                WSL_UNREACHABLE();
+        case WSL_LEX_SPACESEP:
+                while (*c > 0x20)
+                        c++;
+                if (*c != 0x20)
+                        return WSL_LEX_ERROR;
+                r = wsl_lex_outbuf_store(buffer, begin, c);
+                break;
+        case WSL_LEX_BRACKETS:
+                /* required check: this could be the newline */
+                if (*c != 0x5b) {
+                        r = WSL_LEX_ERROR;
+                        break;
+                }
+                c++;
+                while (*c != 0x5d) {
+                        if (*c == 0x0a)
+                                return WSL_LEX_ERROR;
+                        c++;
+                }
+                c++;
+                r = wsl_lex_outbuf_store(buffer, begin+1, c-1);
+                break;
+        default:
+                WSL_UNREACHABLE();
+        }
+        *end_r = c;
+        return r;
+}
+
+enum wsl_lex_retcode
+wsl_lex_token_last(
+        struct wsl_lex_cinfo *info,
+        struct wsl_lex_outbuf *buffer,
+        unsigned char *begin,
+        unsigned char **end_r)
+{
+        int r = WSL_LEX_OK;
+        unsigned char *c = begin;
+
+        switch (info->type) {
+        case WSL_LEX_STOP:
+                while (*c != 0x0a)
+                        c++;
+                break;
+        case WSL_LEX_SPACESEP:
+                while (*c != 0x0a)
+                        c++;
+                r = wsl_lex_outbuf_store(buffer, begin, c);
+                break;
+        case WSL_LEX_BRACKETS:
+                /* required check: this could be the newline */
+                if (*c != 0x5b) {
+                        r = WSL_LEX_ERROR;
+                        break;
+                }
+                c++;
+                while (*c != 0x0a)
+                        c++;
+                r = wsl_lex_outbuf_store(buffer, begin+1, c-1);
+                break;
+        default:
+                WSL_UNREACHABLE();
+        }
+        *end_r = c;
+        return r;
+}
+
+int
+wsl_match_table(
+        unsigned char *name,
+        unsigned char *begin,
+        unsigned char **end_r)
+{
+        unsigned char *n = name;
+        unsigned char *c = begin;
+
+        while (*n && *n == *c)
+                n++, c++;
+        if (*n || *c != 0x20)
+                /* no match */
+                return 0;
+        *end_r = c;
+        return 1;
+}
+
+int
+wsl_lookup_table(
+        unsigned char **tablenames,
+        unsigned char *line)
+{
+        int i;
+        unsigned char *dummy;
+
+        i = 0;
+        while (tablenames[i] && !wsl_match_table(tablenames[i], line, &dummy))
+                i++;
+
+        return i;
+}
+
+enum wsl_lex_retcode
+wsl_lex_line(
+        unsigned char *tablename,
+        struct wsl_lex_cinfo **cinfo,
+        struct wsl_lex_outbuf **buffers,
+        unsigned char *begin,
+        unsigned char **end_r)
+{
+        int r;
+        unsigned char *c;
+        struct wsl_lex_cinfo **ci;
+        struct wsl_lex_outbuf **bu;
+
+        if (!wsl_match_table(tablename, begin, &c))
+                return WSL_LEX_WRONG_TABLE;
+
+        r = WSL_LEX_OK;
+        ci = cinfo;
+        bu = buffers;
+        for (; *ci != NULL; ci++, bu++) {
+                if (*c != 0x20) {
+                        fprintf(stderr, "ERROR! %.32s\n", c);
+                        return WSL_LEX_ERROR;
+                }
+                c++;
+                if (ci[1] == NULL)
+                        r = wsl_lex_token_last(*ci, *bu, c, &c);
+                else
+                        r = wsl_lex_token(*ci, *bu, c, &c);
+                if (r != WSL_LEX_OK) {
+                        return r;
+                }
+        }
+        if (*c != 0x0a)
+                return WSL_LEX_ERROR;
+        *end_r = c + 1;
+        return WSL_LEX_OK;
+}
+
+enum wsl_lex_retcode
+wsl_lex_buffer(
+        unsigned char **tablenames,
+        struct wsl_lex_cinfo ***cinfo,
+        struct wsl_lex_outbuf ***buffers,
+        unsigned char *begin,
+        unsigned char *end,
+        unsigned char **begin_r)
+{
+        int r;
+        size_t t;
+        unsigned char *pos = begin;
+
+        while (pos < end) {
+
+                if (*pos == 0x0a || *pos == 0x25) {
+                        while (*pos != 0x0a)
+                                pos++;
+                        pos++;
+                        continue;
+                }
+
+                t = wsl_lookup_table(tablenames, pos);
+                if (tablenames[t] == NULL)
+                        /* TODO: more descriptive */
+                        return WSL_LEX_ERROR;
+
+                r = wsl_lex_line(tablenames[t], cinfo[t], buffers[t],
+                                 pos, &pos);
+
+                if (r != WSL_LEX_OK)
+                        return r;
+        }
+
+        *begin_r = pos;
+        return WSL_LEX_OK;
+}
+
+#include <stdio.h>
+
+struct wsl_lex_cinfo spacesep = { WSL_LEX_SPACESEP };
+struct wsl_lex_cinfo bracketsep = { WSL_LEX_BRACKETS };
+struct wsl_lex_cinfo skip = { WSL_LEX_STOP };
+
+/* some buffers, use for free */
+struct wsl_lex_outbuf buf[16];
+struct wsl_lex_outbuf *bufarray[] = { &buf[0], &buf[1], &buf[2], &buf[3], &buf[4], &buf[5], &buf[6], &buf[7] };
+struct wsl_lex_outbuf *empty[] = { NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL };
+
+/* baden-wuerttemberg dataset */
+struct wsl_lex_cinfo *info_way[] = { /* &skip, NULL, */ &spacesep, NULL };
+struct wsl_lex_cinfo *info_node[] = { /* &skip, NULL, */ &spacesep, &spacesep, &spacesep, NULL };
+struct wsl_lex_cinfo *info_waypoint[] = { /*&skip, NULL, */ &spacesep, &spacesep, &spacesep, NULL };
+
+char *bw_tables[] = {
+        "way", "node", "waypoint", NULL
+};
+
+struct wsl_lex_cinfo **bw_cinfo[] = {
+        info_way, info_node, info_waypoint
+};
+
+struct wsl_lex_outbuf **bw_buffers[] = {
+        empty, empty, empty,
+        bufarray, bufarray, bufarray
+};
+
+
+/* world dataset */
+struct wsl_lex_cinfo *info_country[] = { &spacesep, &spacesep, &bracketsep, NULL };
+struct wsl_lex_cinfo *info_city[] = { &spacesep, &bracketsep, &spacesep, &bracketsep, &spacesep, NULL };
+struct wsl_lex_cinfo *info_capital[] = { &spacesep, &spacesep, NULL };
+struct wsl_lex_cinfo *info_language[] = { &spacesep, &bracketsep, &spacesep, &spacesep, NULL };
+
+char *world_tables[] = {
+        "Country", "City", "Capital", "Language", NULL
+};
+
+struct wsl_lex_cinfo **world_cinfo[] = {
+        info_country, info_city, info_capital, info_language
+};
+
+struct wsl_lex_outbuf **world_buffers[] = {
+        empty, empty, empty, empty,
+        bufarray, bufarray, bufarray, bufarray
+};
+
+#define BUFSIZE (16*(size_t)4096)
+
+int main(void)
+{
+        int r;
+        size_t size, end;
+        size_t nread;
+        unsigned char buf[BUFSIZE];
+        unsigned char *lexpos;
+
+        size = 0;
+        end = 0;
+
+        for (;;) {
+                size = size - end;
+                memmove(&buf[0], &buf[end], size);
+                nread = fread(&buf[size], 1, BUFSIZE-size, stdin);
+                size += nread;
+
+                if (size == 0)
+                        break;
+
+                end = size;
+                while (end > 0 && buf[end-1] != 0x0a)
+                        end--;
+
+                if (end == 0) {
+                        fprintf(stderr, "Line did not fit in buffer!\n");
+                        fprintf(stderr, "ERROR: Handling this situation not implemented!\n");
+                        exit(1);
+                }
+
+#if 0
+                r = wsl_lex_buffer((unsigned char **)world_tables,
+                                   world_cinfo, world_buffers,
+                                   &buf[0], &buf[end], &lexpos);
+#else
+                r = wsl_lex_buffer((unsigned char **)bw_tables,
+                                   bw_cinfo, bw_buffers,
+                                   &buf[0], &buf[end], &lexpos);
+#endif
+
+                if (r != WSL_LEX_OK) {
+                        fprintf(stderr, "lexer returned %d\n", r);
+                        exit(1);
+                }
+        }
+        return 0;
+}
---
+++ lex.h
@@ -0,0 +1,151 @@
+#include <stddef.h>  /* size_t */
+
+/**
+ * The size of a lex output buffer
+ *
+ * While a table is lexed, each column's values parses into its own output
+ * buffer. The size of this buffer controls how much row data can be lexed
+ * before the output needs to be processed.
+ */
+#define WSL_LEX_OUTBUF_SIZE (16*4096)
+
+/**
+ * Lex buffer for values of a column.
+ *
+ * If a lexem unexpectedly is too big to fit in the remaining space,
+ * a big enough extra buffer is dynamically allocated and the lexec
+ * is copied to it.
+ */
+struct wsl_lex_outbuf {
+        unsigned char buf[WSL_LEX_OUTBUF_SIZE];
+        size_t size;
+        unsigned char *bigbuf;
+        size_t bigsize;
+};
+
+/**
+ * Lexical element types
+ *
+ * @WSL_LEX_STOP: Special symbol; signals skipping the rest of the line
+ * @WSL_LEX_SPACESEP: Space (or newline) delimited token
+ * @WSL_LEX_BRACKETS: Angle bracket delimited token; [looks like this]
+ */
+enum wsl_lex_coltype {
+        WSL_LEX_STOP = 0,
+        WSL_LEX_SPACESEP = 1,
+        WSL_LEX_BRACKETS = 2
+};
+
+/**
+ * Lexical parsing info for a table column
+ *
+ * @type: The lexer type to use.
+ */
+struct wsl_lex_cinfo {
+        enum wsl_lex_coltype type;
+};
+
+/**
+ * Metadata indicating how to lex a table
+ *
+ * @name: Name of the table
+ * @cinfo: A NULL-terminated array of wsl_lex_cinfo.
+ */
+struct wsl_lex_tinfo {
+        const char *name;
+        struct wsl_lex_cinfo **cinfo;
+};
+
+/*
+ * @WSL_LEX_OK: Everything lexed as expected
+ * @WSL_LEX_OUTBUF_FULL: At least one of the output buffers is full
+ * @WSL_LEX_WRONG_TABLE: Parsing was stopped because another table found
+ * @WSL_LEX_ERROR: An error in the input was detected
+ * @WSL_LEX_OOM: Dynamic memory allocation failed
+ */
+enum wsl_lex_retcode {
+        WSL_LEX_OK = 0,
+        WSL_LEX_OUTBUF_FULL = 1,
+        WSL_LEX_WRONG_TABLE = 2,
+        WSL_LEX_ERROR = 3,
+        WSL_LEX_OOM = 4
+};
+
+/**
+ * Lex a single token
+ *
+ * @info: What kind of token to lex.
+ * @buffer: Where to store the lexem, or NULL
+ * @begin: Where to start lexing. This buffer must be terminated by a newline
+ *      character
+ * @end_r: If lexing was successful, the address of the first unconsumed
+ *      character is stored here.
+ */
+int wsl_lex_token(struct wsl_lex_cinfo *info,
+                  struct wsl_lex_outbuf *buffer,
+                  unsigned char *begin,
+                  unsigned char **end_r);
+
+/**
+ * Lex the last token in a line
+ *
+ * @info: What kind of token to lex.
+ * @buffer: Where to store the lexem, or NULL
+ * @begin: Where to start lexing. This buffer must be terminated by a newline
+ *      character
+ * @end_r: If lexing was successful, the address of the first unconsumed
+ *      character is stored here.
+ *
+ * This works like wsl_lex_token(), except that the token is expected to be
+ * followed by the terminating newline character (instead of space).
+ */
+int wsl_lex_token_last(struct wsl_lex_cinfo *info,
+                       struct wsl_lex_outbuf *buffer,
+                       unsigned char *begin,
+                       unsigned char **end_r);
+
+/**
+ * Lex a line of tokens separated by single spaces.
+ *
+ * @tablename: Name of the table of which a row is expected.
+ * @info: NULL-terminated array of cinfo pointers corresponding to given table.
+ * @buffer: Array of buffer pointers corresponding to the columns in the given
+ *      table. Buffer pointers may be NULL in which case the lexems are
+ *      discarded.
+ * @begin: Pointer to beginning of the line to be lexed. Line must be
+ *      terminated by a newline character.
+ * @begin_r: If lexing was successful, the address of the character following
+ *      the terminating newline character is stored here.
+ */
+enum wsl_lex_retcode
+wsl_lex_line(
+        unsigned char *tablename,
+        struct wsl_lex_cinfo **info,
+        struct wsl_lex_outbuf **buffer,
+        unsigned char *begin,
+        unsigned char **end_r);
+
+/**
+ * Lex multiple rows of given table and copy lexems to buffers.
+ *
+ * @tablenames: Array of the names of all tables
+ * @info: For each table name an array of cinfo pointers.
+ * @buffer: For each table name an array of buffer pointers. Buffer pointers
+ *      may be NULL in which case the lexems are discarded.
+ * @begin: Pointer to the beginning of the first line that should be lexed.
+ * @end: Pointer to the byte one past the end of the last line that should be
+ *      lexed (newline character + 1). It must hold `begin <= end`.
+ * @begin_r: On exit, the address of the byte one past the end of the last
+ *      line that was successfully lexed is stored in this pointer (or `begin`
+ *      if no line was lexed successfully).
+ *
+ * Return: All values of `enum wsl_lex_retcode` are possible return values.
+ */
+enum wsl_lex_retcode
+wsl_lex_buffer(
+        unsigned char **tablenames,
+        struct wsl_lex_cinfo ***info,
+        struct wsl_lex_outbuf ***buffer,
+        unsigned char *begin,
+        unsigned char *end,
+        unsigned char **begin_r);
